fe3f36209055bbf896d0cde66376491ef35473e8
Tarek Habash <tarek.soliman797@gmail.com>
2023-09-10 19:56:45 +0200

    load data on startup

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,17 @@
 const http = require("http");
 const app = require("./app.js");
 
+const {loadPlanetsData} = require("./models/planets.model.js")
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`listening on port ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetsData()
+  server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`);
+  });
+}
+
+startServer()
