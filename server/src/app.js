66015ab747d8ebcd796aa50486cedc125df3ba99
Tarek Habash <tarek.soliman797@gmail.com>
2023-09-12 21:53:31 +0200

    serve React app in production with node

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 //separate the express middleware
 const express = require("express");
+const path = require('path')
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
@@ -12,6 +13,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
 
 module.exports = app;
