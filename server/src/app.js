428efb35389b5a70b98297e407e085d6889c9886
Tarek Habash <tarek.soliman797@gmail.com>
2023-09-12 23:39:13 +0200

    serving index.html with express

--- server/src/app.js
+++ server/src/app.js
@@ -20,4 +20,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
 module.exports = app;
