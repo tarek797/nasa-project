3152d7dcec067ace68f19794a53058f6b754f6c1
Tarek Habash <tarek.soliman797@gmail.com>
2023-09-26 22:02:59 +0200

    server side Validation for Post requests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,19 +1,35 @@
-const {getAllLaunches,
-addNewLaunch,
-} = require("../../models/launches.model")
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
-  return res.status(200).json(Array.from(getAllLaunches()))
+  return res.status(200).json(Array.from(getAllLaunches()));
 }
 
-function httpAddNewLaunch(req, res){
-  const launch = req.body
-  launch.launchDate = new Date(launch.launchDate)
-  addNewLaunch(launch)
-  return res.status(201).json(launch)
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+  launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
   httpGetAllLaunches,
-  httpAddNewLaunch
-}
+  httpAddNewLaunch,
+};
